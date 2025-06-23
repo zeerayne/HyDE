ea002acde48c6cb58bbf0139720e3cb4999e63dd
Ilya Bogdanov <zeerayne1337@gmail.com>
2025-06-23 15:23:41 +0300

    fix: move `eza` and `duf` to pkg_extra for fish shell

---
+++ Configs/.config/fish/functions/df.fish
@@ -0,0 +1,15 @@
+function df
+    if type -q duf
+        if test $argv
+            set last_arg $argv[-1]
+            if test -e "$last_arg"
+                duf "$last_arg"
+                return
+            end
+        end
+
+        duf
+    else
+        df $argv
+    end
+end
