7488de1da44191e2fde28ceec3e7e36a944efdaf
Ilya Bogdanov <zeerayne1337@gmail.com>
2025-06-24 21:35:45 +0300

    zsh: tuned
    zsh: sync `user.zsh`

--- Configs/.config/zsh/user.zsh
+++ Configs/.config/zsh/user.zsh
@@ -1,27 +1,23 @@
 #  Startup 
 # Commands to execute on startup (before the prompt is shown)
 # Check if the interactive shell option is set
-if [[ $- == *i* ]]; then
-    # This is a good place to load graphic/ascii art, display system information, etc.
-    if command -v pokego >/dev/null; then
-        pokego --no-title -r 1,3,6
-    elif command -v pokemon-colorscripts >/dev/null; then
-        pokemon-colorscripts --no-title -r 1,3,6
-    elif command -v fastfetch >/dev/null; then
-        if do_render "image"; then
-            fastfetch --logo-type kitty
-        fi
-    fi
-fi
 
 #  Plugins 
 # manually add your oh-my-zsh plugins here
 plugins=(
     "sudo"
+    "zsh-256color"
+    "zsh-autosuggestions"
+    "zsh-syntax-highlighting"
+    "zsh-completions"
+    "zsh-history-substring-search"
+    "fzf-tab"
 )
 
 #   Overrides 
-# unset HYDE_ZSH_NO_PLUGINS # Set to 1 to disable loading of oh-my-zsh plugins, useful if you want to use your zsh plugins system 
-# unset HYDE_ZSH_PROMPT # Uncomment to unset/disable loading of prompts from HyDE and let you load your own prompts
-# HYDE_ZSH_COMPINIT_CHECK=1 # Set 24 (hours) per compinit security check // lessens startup time
+# unset HYDE_ZSH_DEFER # Uncomment to disable deferred loading of zsh and let you load you plugin YOURSELF example using zinit
 # HYDE_ZSH_OMZ_DEFER=1 # Set to 1 to defer loading of oh-my-zsh plugins ONLY if prompt is already loaded
+unset HYDE_ZSH_PROMPT # Uncomment to unset/disable loading of prompts from HyDE and let you load your own prompts
+HYDE_ZSH_COMPINIT_CHECK=24 # Set 24 (hours) per compinit security check // lessens startup time
+ZSH_THEME=""
+DISABLE_AUTO_TITLE="true"
