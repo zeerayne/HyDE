8d8ca27ef5b8e9c0d2123449ef5a32ed32aa2ab7
post2424 <markus.post@ut.ee>
2026-01-10 08:00:10 +0200

    Refactor gamemode.sh to use hyde-shell workflows
    Beforehand using this script would only apply opacity to new windows.
    Now when using this script it applies opacity automatically.

--- Configs/.local/lib/hyde/gamemode.sh
+++ Configs/.local/lib/hyde/gamemode.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '1p' | awk '{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
-    hyprctl -q --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:xray 1;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0 ;\
-        keyword decoration:active_opacity 1 ;\
-        keyword decoration:inactive_opacity 1 ;\
-        keyword decoration:fullscreen_opacity 1 ;\
-        keyword layerrule noanim,waybar ;\
-        keyword layerrule noanim,swaync-notification-window ;\
-        keyword layerrule noanim,swww-daemon ;\
-        keyword layerrule noanim,rofi
-        "
-    hyprctl "keyword windowrule opaque on, match:class .*"
-    exit
+    hyde-shell workflows --set gaming
 else
-    hyprctl reload config-only -q
+    hyde-shell workflows --set default
 fi
