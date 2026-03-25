74a2f316f5f4f030d3bc1c1f54eff5639a2bbd43
Khing <alampayk@gmail.com>
2026-03-25 17:24:55 +0800

    Porting to awww

--- Configs/.local/lib/hyde/wallpaper.awww.sh
+++ Configs/.local/lib/hyde/wallpaper.awww.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-#! A fallback script. Incase user explcitly set swww as wallpaper backend
-notify-send "" "DEPRECATION NOTICE: swww backend is deprecated, please switch to awww or other supported backends. See 'hyde-shell wallpaper --help' for more info."
-
 selected_wall="${1:-"$$HYDE_CACHE_HOME/wall.set"}"
 lockFile="$XDG_RUNTIME_DIR/hyde/$(basename "$0").lock"
 if [ -e "$lockFile" ]; then
@@ -20,21 +16,21 @@ scrDir="$(dirname "$(realpath "$0")")"
 source "$scrDir/globalcontrol.sh"
 case "$WALLPAPER_SET_FLAG" in
     p)
-        xtrans=$WALLPAPER_SWWW_TRANSITION_PREV
+        xtrans=$WALLPAPER_AWWW_TRANSITION_PREV
         xtrans="${xtrans:-"outer"}"
         ;;
     n)
-        xtrans=$WALLPAPER_SWWW_TRANSITION_NEXT
+        xtrans=$WALLPAPER_AWWW_TRANSITION_NEXT
         xtrans="${xtrans:-"grow"}"
         ;;
 esac
 selected_wall="$1"
 [ -z "$selected_wall" ] && echo "No input wallpaper" && exit 1
 selected_wall="$(readlink -f "$selected_wall")"
-if ! swww query &> /dev/null; then
-    swww-daemon --format xrgb &
+if ! awww query &> /dev/null; then
+    awww-daemon --format xrgb &
     disown
-    swww query && swww restore
+    awww query && awww restore
 fi
 is_video=$(file --mime-type -b "$selected_wall" | grep -c '^video/')
 if [ "$is_video" -eq 1 ]; then
@@ -44,9 +40,9 @@ if [ "$is_video" -eq 1 ]; then
     extract_thumbnail "$selected_wall" "$cached_thumb"
     selected_wall="$cached_thumb"
 fi
-xtrans=$WALLPAPER_SWWW_TRANSITION_DEFAULT
+xtrans=$WALLPAPER_AWWW_TRANSITION_DEFAULT
 [ -z "$xtrans" ] && xtrans="grow"
 [ -z "$wallFramerate" ] && wallFramerate=60
 [ -z "$wallTransDuration" ] && wallTransDuration=0.4
 print_log -sec "wallpaper" -stat "apply" "$selected_wall"
-swww img "$(readlink -f "$selected_wall")" --transition-bezier .43,1.19,1,.4 --transition-type "$xtrans" --transition-duration "$wallTransDuration" --transition-fps "$wallFramerate" --invert-y --transition-pos "$(hyprctl cursorpos | grep -E '^[0-9]' || echo "0,0")" &
+awww img "$(readlink -f "$selected_wall")" --transition-bezier .43,1.19,1,.4 --transition-type "$xtrans" --transition-duration "$wallTransDuration" --transition-fps "$wallFramerate" --invert-y --transition-pos "$(hyprctl cursorpos | grep -E '^[0-9]' || echo "0,0")" &
