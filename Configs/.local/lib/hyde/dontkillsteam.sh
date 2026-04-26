e94a0f904a59a161469d0a055e5a1e44d5051f2d
Shiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-04-26 15:53:24 -0500

    fix: Deprecate dontkillsteam (#1710)
    Co-authored-by: Khing <53417443+kRHYME7@users.noreply.github.com>

--- Configs/.local/lib/hyde/dontkillsteam.sh
+++ Configs/.local/lib/hyde/dontkillsteam.sh
@@ -1,3 +1,6 @@
+# NOTE: This script is now deprecated as hyprland no longer fully kills steam!
+# You shouldn't need to use it in any of your keybindings!
+
 if [[ $(hyprctl activewindow -j | jq -r ".class") == "Steam" ]]; then
     xdotool windowunmap $(xdotool getactivewindow)
 else
