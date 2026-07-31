8bfcf873a7d4534dd9e7061aeefe71066542c497
Ilya Bogdanov <zeerayne1337@gmail.com>
2026-07-31 20:47:53 +0300

    no starship

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -362,8 +362,6 @@ EOF
 		[[global.dependency]]
 		pacman = [
 	TOML
-	# starship is needed for both zsh and fish prompts
-	echo "\"starship\"," >> "${extra_toml}"
 	echo "]" >> "${extra_toml}"
 
 	#-------------------------------#
